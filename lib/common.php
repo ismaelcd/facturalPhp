2e70c5c4dc9a487ed179367c3c0c25c9aaed5e42
ismaelcd <ismaelcd@gmail.com>
2013-07-19 09:27:23 +0200

    update common.php

--- lib/common.php
+++ lib/common.php
@@ -1,5 +1,13 @@
 <?php
 
-	// Path
+	// Creamos la ruta absoluta del sitio
+    $INFUENTE_ROOT = "/var/www/facturalPhp";
+    
+    // Cargamos la conexión SQL y PostGres al servidor
+    require $INFUENTE_ROOT.'/lib/conn.php';
+    //require $INFUENTE_ROOT.'/lib/pg_conn.php';
+    
+    // Cargamos el header
+    require $INFUENTE_ROOT.'/lib/header.php';
 
 ?>
